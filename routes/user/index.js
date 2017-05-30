0e94bc799893566e1e0f3ff01298706b2ee89da9
Maksim Shuba <pro100stalkerss@gmail.com>
2017-05-30 21:43:13 +0300

    Add user favorite route

--- routes/user/index.js
+++ routes/user/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const info = require('./info');
+const favorite = require('./favorite');
 
 router.get('/info', info.get);
+router.get('/favorite', favorite.get);
 
 module.exports = router;
